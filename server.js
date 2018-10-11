7d52ce46b10ebb24f5960307bc5e58a7b4541c76
Tori Machen <tmachen@pillartechnology.com>
2018-10-11 14:42:38 -0500

    can make request and get respone using SOAP

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 'use strict';
 
+const http = require('http');
+const soap = require('soap');
 const express = require('express');
+const bodyParser = require('body-parser');
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+var myService = {
+      MyService: {
+          MyPort: {
+              MyFunction: function(args) {
+                  return {
+                      name: args.name,
+                      testParam: args.testParam
+                  };
+              }
+          }
+      }
+  };
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Welcome! To my Kata! :)\n');
+var xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
+
+//http server example
+var server = http.createServer(function(request,response) {
+    response.end('404: Not Found: ' + request.url);
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+server.listen(8000);
+soap.listen(server, '/wsdl', myService, xml);
+
+//express server example
+var app = express();
+
+//body parser middleware are supported (optional)
+app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
+
+app.listen(8001, function(){
+    //Note: /wsdl route will be handled by soap module
+    //and all other routes & middleware will continue to work
+    soap.listen(app, '/wsdl', myService, xml);
+});
